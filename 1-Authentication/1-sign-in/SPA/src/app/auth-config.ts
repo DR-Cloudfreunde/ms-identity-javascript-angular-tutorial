e3ab046af9a0433872768fc9018523d6e123cb85
DR-Cloudfreunde <114481600+DR-Cloudfreunde@users.noreply.github.com>
2024-03-28 11:15:38 +0100

    Update auth-config.ts

--- 1-Authentication/1-sign-in/SPA/src/app/auth-config.ts
+++ 1-Authentication/1-sign-in/SPA/src/app/auth-config.ts
@@ -16,8 +16,8 @@ const isIE = window.navigator.userAgent.indexOf("MSIE ") > -1 || window.navigato
  */
 export const msalConfig: Configuration = {
     auth: {
-        clientId: 'Enter_the_Application_Id_Here', // This is the ONLY mandatory field that you need to supply.
-        authority: 'https://login.microsoftonline.com/Enter_the_Tenant_Info_Here', // Defaults to "https://login.microsoftonline.com/common"
+        clientId: '8288641f-1361-42e1-9612-17eb675a63c6', // This is the ONLY mandatory field that you need to supply.
+        authority: 'https://login.microsoftonline.com/2e96bb73-9ff7-4660-be52-1991daa901ab', // Defaults to "https://login.microsoftonline.com/common"
         redirectUri: '/', // Points to window.location.origin. You must register this URI on Azure portal/App Registration.
         postLogoutRedirectUri: '/', // Indicates the page to navigate after logout.
         navigateToLoginRequestUrl: true, // If "true", will navigate back to the original request location before processing the auth code response.
